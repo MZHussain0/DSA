2f64dc88a6823a34b2b5bba541e4e582c549394c
mzhussain0 <mzhussain092@gmail.com>
2023-11-06 22:52:58 +0530

    interview problems

--- 2_Strings/easy/13.indexOfFirstOccurance.js
+++
@@ -1,8 +0,0 @@
-﻿/**
- * Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
- */
-
-const strStr = (haystack, needle) => {
-  if (!needle) return 0;
-  return haystack.indexOf(needle);
-};
